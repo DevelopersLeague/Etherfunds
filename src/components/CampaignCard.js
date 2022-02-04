d4d42ce2c1286a470394e1d83a8bf2fd24a45dae
Shyren <shyren.more30@gmail.com>
2022-02-04 13:07:14 +0530

    complete landing page

---
+++ src/components/CampaignCard.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import {
+    Heading,
+    useColorModeValue,
+    Text,
+    Flex,
+    Box,
+    Img,
+    Icon,
+    chakra,
+    Tooltip,
+    Progress,
+} from "@chakra-ui/react";
+
+const CampaignCard = ({name, description, creatorId, imageURL, id, balance, target, ethPrice}) => {
+
+    return (
+        // pls add a Navlink component
+        <a href={`/campaign/${id}`}>
+            <Box
+                bg={useColorModeValue("white", "gray.800")}
+                maxW={{ md: "sm" }}
+                borderWidth="1px"
+                rounded="lg"
+                shadow="lg"
+                position="relative"
+                alignItems="center"
+                justifyContent="center"
+                cursor="pointer"
+                transition={"transform 0.3s ease"}
+                _hover={{
+                    transform: "translateY(-8px)",
+                }}
+            >
+                <Box height="18em">
+                    <Img
+                        src={imageURL}
+                        alt={`Picture of ${name}`}
+                        roundedTop="lg"
+                        objectFit="cover"
+                        w="full"
+                        h="full"
+                        display="block"
+                    />
+                </Box>
+                <Box p="6">
+                    <Flex
+                        mt="1"
+                        justifyContent="space-between"
+                        alignContent="center"
+                        py={2}
+                    >
+                        <Box
+                            fontSize="2xl"
+                            fontWeight="semibold"
+                            as="h4"
+                            lineHeight="tight"
+                            isTruncated
+                        >
+                            {name}
+                        </Box>
+
+                        <Tooltip
+                            label="Click to see description"
+                            bg={useColorModeValue("white", "gray.700")}
+                            placement={"top"}
+                            color={useColorModeValue("gray.800", "white")}
+                            fontSize={"1.2em"}
+                        >
+                            <chakra.a display={"flex"}>
+                                <Icon
+                                    as="🤝"
+                                    h={7}
+                                    w={7}
+                                    alignSelf={"center"}
+                                    color={"teal.400"}
+                                />{" "}
+                            </chakra.a>
+                        </Tooltip>
+                    </Flex>
+                    <Flex alignContent="center" py={2}>
+                        {" "}
+                        <Text color={"gray.500"} pr={2}>
+                            by
+                        </Text>{" "}
+                        <Heading size="base" isTruncated>
+                            {creatorId}
+                        </Heading>
+                    </Flex>
+                    <Flex direction="row" py={2}>
+                        <Box w="full">
+                            <Box
+                                fontSize={"2xl"}
+                                isTruncated
+                                maxW={{ base: "	15rem", sm: "sm" }}
+                                pt="2"
+                            >
+                                {/* <Text as="span" fontWeight={"bold"}>
+                                    {balance > 0
+                                        ? web3.utils.fromWei(balance, "ether")
+                                        : "0, Become a Donor 😄"}
+                                </Text> */}
+                                <Text
+                                    as="span"
+                                    display={balance > 0 ? "inline" : "none"}
+                                    pr={2}
+                                    fontWeight={"bold"}
+                                >
+                                    {" "}
+                                    ETH
+                                </Text>
+                                <Text
+                                    as="span"
+                                    fontSize="lg"
+                                    display={balance > 0 ? "inline" : "none"}
+                                    fontWeight={"normal"}
+                                    color={useColorModeValue("gray.500", "gray.200")}
+                                >
+                                    {/* (${getWEIPriceInUSD(ethPrice, balance)}) */}
+                                </Text>
+                            </Box>
+
+                            {/* <Text fontSize={"md"} fontWeight="normal">
+                                target of {web3.utils.fromWei(target, "ether")}
+                                ETH
+                                (${getWEIPriceInUSD(ethPrice, target)})
+                            </Text> */}
+                            <Progress
+                                colorScheme="teal"
+                                size="sm"
+                                // value={web3.utils.fromWei(balance, "ether")}
+                                // max={web3.utils.fromWei(target, "ether")}
+                                value="30"
+                                max="100"
+                                mt="2"
+                            />
+                        </Box>{" "}
+                    </Flex>
+                </Box>
+            </Box>
+        </a>
+    );
+};
+
+export default CampaignCard;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,41 +1,135 @@
 import React from 'react';
 import FeatureBox from '../components/FeatureBox';
 import {
+    Heading,
+    useBreakpointValue,
+    useColorModeValue,
+    Text,
+    Button,
+    Flex,
+    Container,
     SimpleGrid,
+    Box,
+    Divider,
+    Skeleton,
+    Img,
     Icon,
-    
+    chakra,
+    Tooltip,
+    Link,
+    SkeletonCircle,
+    HStack,
+    Stack,
+    Progress,
 } from "@chakra-ui/react";
+import CampaignCard from '../components/CampaignCard';
 import { FcShare, FcDonate, FcMoneyTransfer } from "react-icons/fc";
+import styles from '../styles/Home.module.css'
 
 const LandingPage = () => {
     return <div>
-        <main>
-            
-        </main>
+        <main className={styles.main}>
+            <Container py={{ base: "4", md: "12" }} maxW={"7xl"} align={"left"}>
+                {" "}
+                <Heading
+                    textAlign={useBreakpointValue({ base: "left" })}
+                    fontFamily={"heading"}
+                    color={useColorModeValue("gray.800", "white")}
+                    as="h1"
+                    py={4}
+                >
+                    Crowdfunding using the powers of <br /> Crypto & Blockchain 😄{" "}
+                </Heading>
 
-        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} py={8}>
-            <FeatureBox
-                icon={<Icon as={FcDonate} w={10} h={10} />}
-                title={"Create a Campaign for Fundraising"}
-                text={
-                    "It’ll take only 2 minutes. Just enter a few details about the funds you are raising for."
-                }
-            />
-            <FeatureBox
-                icon={<Icon as={FcShare} w={10} h={10} />}
-                title={"Share your Campaign"}
-                text={
-                    "All you need to do is share the Campaign with your friends, family and others. In no time, support will start pouring in."
-                }
-            />
-            <FeatureBox
-                icon={<Icon as={FcMoneyTransfer} w={10} h={10} />}
-                title={"Request and Withdraw Funds"}
-                text={
-                    "The funds raised can be withdrawn directly to the recipient when 50% of the contributors approve of the Withdrawal Request."
-                }
-            />
-        </SimpleGrid>
+                {/* Navlink will come here */}
+                <a href="/campaign/new">
+                    <Button
+                        display={{ sm: "inline-flex" }}
+                        fontSize={"md"}
+                        fontWeight={600}
+                        color={"white"}
+                        bg={"teal.400"}
+                        _hover={{
+                            bg: "teal.300",
+                        }}
+                    >
+                        Create Campaign
+                    </Button>
+                </a>
+            </Container>
+
+            <Container py={{ base: "4", md: "12" }} maxW={"7xl"}>
+                <HStack spacing={2}>
+                    <SkeletonCircle size="4" />
+                    <Heading as="h2" size="lg">
+                        Open Campaigns
+                    </Heading>
+                </HStack>
+
+                <Divider marginTop="4" />
+
+                <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} py={8}>
+                <div>
+                    <CampaignCard
+                        name="test fundname"
+                        description="Just testing"
+                        creatorId="Some SHA string"
+                        imageURL="TBD"
+                        id="umm nope"
+                        target="10,000"
+                        balance="10"
+                        ethPrice="10 ETH"
+                    />
+                </div>
+                <div>
+                    <CampaignCard
+                        name="test fundname"
+                        description="Just testing"
+                        creatorId="Some SHA string"
+                        imageURL="TBD"
+                        id="umm nope"
+                        target="10,000"
+                        balance="0"
+                        ethPrice="10 ETH"
+                    />
+                </div>
+                </SimpleGrid>
+            </Container>
+
+            <Container py={{ base: "4", md: "12" }} maxW={"7xl"} id="howitworks">
+                <HStack spacing={2}>
+                    <SkeletonCircle size="4" />
+                    <Heading as="h2" size="lg">
+                        How SmartFundz Works
+                    </Heading>
+                </HStack>
+                <Divider marginTop="4" />
+                <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} py={8}>
+                    <FeatureBox
+                        icon={<Icon as={FcDonate} w={10} h={10} />}
+                        title={"Create a Campaign for Fundraising"}
+                        text={
+                            "It’ll take only 2 minutes. Just enter a few details about the funds you are raising for."
+                        }
+                    />
+                    <FeatureBox
+                        icon={<Icon as={FcShare} w={10} h={10} />}
+                        title={"Share your Campaign"}
+                        text={
+                            "All you need to do is share the Campaign with your friends, family and others. In no time, support will start pouring in."
+                        }
+                    />
+                    <FeatureBox
+                        icon={<Icon as={FcMoneyTransfer} w={10} h={10} />}
+                        title={"Request and Withdraw Funds"}
+                        text={
+                            "The funds raised can be withdrawn directly to the recipient when 50% of the contributors approve of the Withdrawal Request."
+                        }
+                    />
+                </SimpleGrid>
+                
+            </Container>
+        </main>
   </div>;
 };
 
